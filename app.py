ce4208b6109cdcc240efda9843ec51ff1f2ccfd6
Davide Iovino <75441668+davideiov@users.noreply.github.com>
2022-05-18 20:25:09 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import boto3
+from operator import itemgetter
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 
 app = Flask(__name__)
 dynamodb = boto3.resource('dynamodb', endpoint_url="http://localhost:4566")
@@ -21,6 +22,21 @@ for element in db_water:
 		value += int(day)
 	water[int(element['month-year'].split('-')[0]) - 1] = value
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def homepage():
-    return render_template('dashboard.html', fields=db_fields, water=water)
+	if request.method == 'POST':
+		table = dynamodb.Table('Fields')
+		field_id = str(request.form.get('id'))
+		crop_type = str(request.form.get('a'))
+		mqs = str(request.form.get('b'))
+		
+		table.update_item(Key={'id': field_id}, UpdateExpression="set crop_type = :crop_type", ExpressionAttributeValues={':crop_type': crop_type})
+		table.update_item(Key={'id': field_id}, UpdateExpression="set mq_available = :mq_available", ExpressionAttributeValues={':mq_available': mqs})
+	
+		response = table.scan()
+		
+		return render_template('dashboard.html', fields=sorted(response['Items'], key=lambda k : k['id']), water=water)
+		
+	return render_template('dashboard.html', fields=sorted(db_fields, key=lambda k : k['id']), water=water)
+	
+	
